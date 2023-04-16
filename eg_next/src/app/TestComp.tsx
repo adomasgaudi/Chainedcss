6228fe2a7fdab8a23387058ffa0a05c6b9dde86e
Adomas Gaudiesius <adomas.gaudi@gmail.com>
2023-04-16 14:09:59 +0300

    feat: create working example, change license, readme

---
+++ eg_next/src/app/TestComp.tsx
@@ -0,0 +1,13 @@
+'use client';
+/** @jsxImportSource @emotion/react */
+import Link from 'next/link';
+import { _ } from 'chainedcss';
+
+const TestComp = () => (
+  <div>
+    <Link href="/about">helo link</Link>
+    <h1 {..._.bgRed500()}>CHAINS</h1>
+  </div>
+);
+
+export default TestComp;
---
+++ eg_next/src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import TestComp from './TestComp';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1> hi next 2 </h1>
+      <TestComp></TestComp>
+    </main>
+  );
+}
